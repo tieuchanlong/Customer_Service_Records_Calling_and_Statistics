603d9a736dc337419a49078ea8375e6651167f78
Long Tieu <l.tieu@share.epsb.ca>
2018-10-29 14:43:31 -0600

    The file of calling customers and showing customer data are finished.

---
+++ customers_data.py
@@ -0,0 +1,17 @@
+'''
+    This is the data for customers served and left.
+'''
+
+def ask_served(served):
+    print("The number of customers being served is " + len(served) + ".")
+    
+def ask_left(left):
+    print("The number of customers having left is " + len(left) + ".")
+    
+def print_list_served(served):
+    print("The list of customers being served: ")
+    print(served)
+    
+def print_list_left(left):
+    print("The list of customers having left: ")
+    print(left)
